76c64d988bdc10fdbaca5de3b0a023106aecd74e
Ajith Antony <ajithantony2009@gmail.com>
2024-11-14 22:04:16 -0500

    Added React app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { useState, useEffect } from 'react';
+import { HubConnectionBuilder } from '@microsoft/signalr';
 
-export default App;
+const LiveUpdates = () => {
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        const connection = new HubConnectionBuilder()
+            .withUrl('http://localhost:5161/liveupdate/') // URL of your SignalR Hub
+            .build();
+
+        connection.start()
+            .then(() => {
+                console.log('Connected to SignalR');
+            })
+            .catch(err => console.error('Connection failed: ', err));
+
+        connection.on('ReceiveMessage', (message) => {
+            setMessage(message);
+        });
+
+        // Cleanup on unmount
+        return () => {
+            connection.stop();
+        };
+    }, []);
+
+    return (
+        <div>
+            <h2>Live Update: </h2>
+            <p>{message}</p>
+        </div>
+    );
+};
+
+export default LiveUpdates;
+
